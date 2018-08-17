5492034a358603e4921cbd874d742d3921da48a0
Raimundo Vinicius <raimundovsa@gmail.com>
2018-08-17 19:55:47 -0300

    Ajustar o Modal e Função do JS

---
+++ script.js
@@ -0,0 +1,6 @@
+let login = document.querySelector(".loginjs");
+let loginModal = document.querySelector("#loginModal");
+
+login.addEventListener("click",() =>{
+    loginModal.classList.toggle("ativar")
+})
