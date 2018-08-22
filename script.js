561359f5eabd1f676177538a058514995b35b987
Luana Meira <luanameiracfc@gmail.com>
2018-08-22 19:45:04 -0300

    Alterações da funçao do desafio no javascript

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let login = document.querySelector(".loginjs");
 let loginModal = document.querySelector("#loginModal");
 
+
 login.addEventListener("click",() =>{
     loginModal.classList.toggle("ativar")
 })
